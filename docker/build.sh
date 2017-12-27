fd4b438f93a5f3d3abaadc4bc34c3f7b3047e803
Wojciech Mleczek <mleczek.wojtek@gmail.com>
2017-12-27 22:55:46 +0100

    Docker push via Travis CI

---
+++ docker/build.sh
@@ -0,0 +1,13 @@
+# Get first argument (deplink version),
+# or use default ("master") if not set.
+tag=$1
+version=$1
+if [ -z "$version" ]; then
+    tag=latest
+    version=master
+fi;
+
+# Create Docker image
+docker build https://github.com/deplink/deplink.git#$version -t deplink:$tag --rm
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+docker push deplink/deplink:$tag
