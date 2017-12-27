da2541a5f0920183a0eff6f66fbf646af342eb7c
Wojciech Mleczek <mleczek.wojtek@gmail.com>
2017-12-28 00:37:20 +0100

    Test new deployment scripts

--- docker/build.sh
+++ docker/build.sh
@@ -8,6 +8,6 @@ if [ -z "$version" ]; then
 fi;
 
 # Create Docker image
-docker build https://github.com/deplink/deplink.git#$version -t deplink:$tag --rm
+docker build https://github.com/deplink/deplink.git#$version -t deplink/deplink:$tag --rm
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push deplink/deplink:$tag
